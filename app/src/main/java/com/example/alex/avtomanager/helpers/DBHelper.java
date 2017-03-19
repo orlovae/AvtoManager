882b9ea8c64595ae7516f80467b4b748e7adef4d
Alexander Orlov <79202875363@ya.ru>
2017-03-19 18:01:43 +0300

    Привет в порядок xml файлы.

---
+++ app/src/main/java/com/example/alex/avtomanager/helpers/DBHelper.java
@@ -0,0 +1,30 @@
+package com.example.alex.avtomanager.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by alex on 19.03.17.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "DBNote";
+    private static final int DATABASE_VERSION = 1; // версия базы данных
+
+
+    public DBHelper (Context context){
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/alex/avtomanager/tables/ParameterTable.java
@@ -0,0 +1,8 @@
+package com.example.alex.avtomanager.tables;
+
+/**
+ * Created by alex on 19.03.17.
+ */
+
+public class ParameterTable {
+}
